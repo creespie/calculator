b45fc79e9f030481e4d957d32252ea8666496a01
creespie <150524904+creespie@users.noreply.github.com>
2024-07-23 11:08:50 +0200

    add html boilerplate and basic pseudocode

---
+++ script.js
@@ -0,0 +1,7 @@
+//pseudocode:
+// create an array storing the current number and the old number as strings
+//need a function related to all numbers on the calculator to add them on the current number
+//a function related to operators to shift the current number to the older number and save the operator selected
+//a function for the equal to complete the operation with the operator selected, return the result as the new number and delete the old number
+//things to care about: the dot can be present once in every number, 
+//using an operator while having one selected should execute the operation and store the result as the old number
